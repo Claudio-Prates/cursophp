bb1ecca79b5e455d88bff7f0643e2f98ef4aa09a
Claudio-Prates <93285417+Claudio-Prates@users.noreply.github.com>
2023-03-19 02:26:32 -0300

    Add files via upload

---
+++ DESAFIOS/D002/Numeros.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+
+        <h1>Trabalhando com múmeros aleatórios</h1>
+<?php 
+
+    $numero = rand(0,101);
+
+        echo "Gerando um número aleatório entre 0 e 100...
+         <br> O número sorteado foi <strong>$numero</strong>";
+
+
+    
+
+?>
+
+
+
+        
+<button onclick="location.reload()">&#x1F504 Gerar Outro</button>
+
+
+
+    
+    </main>
+</body>
+
+</html>
