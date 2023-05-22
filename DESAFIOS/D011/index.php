4d34977c046fdd55494e44b50d335af6c9e60899
Claudio-Prates <cpratesjunior@bol.com.br>
2023-05-22 13:58:37 -0300

    aaaaa

--- DESAFIOS/D011/index.php
+++ DESAFIOS/D011/index.php
@@ -1,43 +1,45 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Reajustador de Preço</title>
-</head>
-<body>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-		<label for="valor">Valor do produto:</label>
-		<input type="text" name="valor" id="valor" required><br><br>
-		<label for="porcentagem">Porcentagem de reajuste:</label>
-		<input type="range" name="porcentagem" id="porcentagem" min="0" max="100" value="0" step="1" onchange="atualizarValor()"><br><br>
-		<label for="novo_valor">Novo valor:</label>
-		<input type="text" name="novo_valor" id="novo_valor" readonly><br><br>
-		<input type="submit" value="Calcular">
-	</form>
+  <head>
+    <title>Reajustador de Preço</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    
+  <?php
+            
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $valor = $_POST["valor"];
+                $porcentagem = $_POST["porcentagem"];
+                $novo_valor = $valor * (1 + $porcentagem / 100);
+                
+            }
+            ?>
+    <main>
+    <h1>Reajustador de Preço</h1>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <label for="valor">Preço do Produto (R$)</label>
+      <input type="text" id="valor" name="valor" required>
+      <label for="porcentagem">Qual será o percentual de reajuste <strong>(<?php echo $porcentagem;?>%):</strong></label>
 
-	<script>
-		function atualizarValor() {
-			var valor = document.getElementById("valor").value;
-			var porcentagem = document.getElementById("porcentagem").value;
-			var novo_valor = (valor * (1 + (porcentagem / 100))).toFixed(2);
-			document.getElementById("novo_valor").value = novo_valor;
-		}
-	</script>
+      <input type="range" id="porcentagem" name="porcentagem" min="0" max="100" value="0" onchange="updateTextInput(this.value);">
+      <input type="text" id="porcentagem_text" name="porcentagem_text" value="0%" readonly>
+      <input type="submit" name="submit" value="Reajustar">
+    </form>
+    </main>
+
     <section>
-    <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if(isset($_POST["valor"]) && isset($_POST["porcentagem"])){
-		$valor = $_POST["valor"];
-		$porcentagem = $_POST["porcentagem"];
-		$novo_valor = $valor * (1 + ($porcentagem / 100));
-		echo "O Produto que custava R$ " . number_format($valor, 2, ',', '.') . ", com um aumento de " . $porcentagem . "%, vai passar a custar R$ " . number_format($novo_valor, 2, ',', '.') . ".";
-	} else {
-		echo "Preencha os campos corretamente.";
-	}
-}
-?>        
-        
+    <h2>Resultado do Reajuste</h2>
+        <?php
             
         
+            echo "O Produto que custava <strong> R$ " . number_format($valor, 2, ',', '.') . "</strong>, com um aumento de <strong>" . $porcentagem . "%  de aumento </strong>, vai passar a custar R$ " . number_format($novo_valor, 2, ',', '.') . ".";
+        
+        ?>
+    <script>
+      function updateTextInput(val) {
+        document.getElementById('porcentagem_text').value = val + "%";
+      }
+    </script>
 </section>
-</body>
+  </body>
 </html>
