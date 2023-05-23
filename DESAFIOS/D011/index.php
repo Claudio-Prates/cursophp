e5c639bf616cdab16ad772d7420d64949d63ee2d
Claudio-Prates <93285417+Claudio-Prates@users.noreply.github.com>
2023-05-23 20:16:49 +0100

    Update index.php

--- DESAFIOS/D011/index.php
+++ DESAFIOS/D011/index.php
@@ -10,10 +10,10 @@
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $valor = $_POST["valor"];
                 $porcentagem = $_POST["porcentagem"];
-                $novo_valor = $valor * (1 + $porcentagem / 100);
+                $novo_valor = $valor * (1 + $porcentagem/ 100);
                 
             }
-            ?>
+            ?>  
     <main>
     <h1>Reajustador de Preço</h1>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
@@ -25,14 +25,15 @@
       <input type="text" id="porcentagem_text" name="porcentagem_text" value="0%" readonly>
       <input type="submit" name="submit" value="Reajustar">
     </form>
-    </main>
+    </main> 
 
     <section>
     <h2>Resultado do Reajuste</h2>
         <?php
             
         
-            echo "O Produto que custava <strong> R$ " . number_format($valor, 2, ',', '.') . "</strong>, com um aumento de <strong>" . $porcentagem . "%  de aumento </strong>, vai passar a custar R$ " . number_format($novo_valor, 2, ',', '.') . ".";
+            echo "O Produto que custava <strong> R$ " . number_format($valor, 2, ',', '.') . "</strong>, 
+            com um aumento de <strong>" . $porcentagem . "%  de aumento </strong>, vai passar a custar R$ <strong>" . number_format($novo_valor, 2, ',', '.') ."</strong>.";
         
         ?>
     <script>
